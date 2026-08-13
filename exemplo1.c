fb1257f8da1a07f5df37b78f3c2f5f510b5db274
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2026-08-13 19:57:13 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,14 @@
+/* Lab. 1
+  * Exemplo 1* /
+
+#include <studio.h>
+
+int main() {
+  //Imprime algumas mensagens na tela
+  printf("Mackenzie FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+
+
+  return 0
+}
